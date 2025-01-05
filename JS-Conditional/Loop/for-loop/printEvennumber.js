a5fb80e440062c260226e33fad81070d7591d69b
Rudra swain <rudrasankar96@gmail.com>
2025-01-05 12:58:10 +0530

    Print even numbers using for loop

---
+++ JS-Conditional/Loop/for-loop/printEvennumber.js
@@ -0,0 +1,9 @@
+// Even Numbers:
+// Write a program to print all even numbers between 1 and 50 using a for loop.
+
+let i=0;
+for(i=1; i<=50;i++){
+    if(i % 2 === 0){
+        console.log('the even numers are', i);
+    }
+}
