c86587b7bbaf7cd2a51a92ecb6cbfe06bc04d9ab
Rudra swain <rudrasankar96@gmail.com>
2025-01-05 12:53:01 +0530

    Sum of all numbers using for loop

---
+++ JS-Conditional/Loop/for-loop/sumOfnumbers.js
@@ -0,0 +1,9 @@
+// Sum of Numbers:
+// Write a program to find the sum of numbers from 1 to 100 using a for loop.
+
+let i=0;
+let sum=0;
+for(i=1;i<=100;i++){
+    sum=sum+i;
+}
+console.log('the sum of all number is',sum);
