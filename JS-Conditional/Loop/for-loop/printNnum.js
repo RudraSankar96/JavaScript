b3a5deb59ef2cb90dce4cdac6ba21e3a27bf2590
Rudra swain <rudrasankar96@gmail.com>
2025-01-05 11:36:51 +0530

    Print N number using for loop

---
+++ JS-Conditional/Loop/for-loop/printNnum.js
@@ -0,0 +1,14 @@
+// Print Numbers:
+// Write a program to print numbers from 1 to 10 using a for loop.
+
+let i;
+for( i=1; i<=10; i++){
+    console.log(i);
+}
+
+// Write a program to print numbers from 1 to 500 using a for loop.
+
+let n;
+for( n=1; n<=50; n++){
+    console.log(n);
+}
