5189d2ddf2380274f47cc551ed342de28313ae9e
Rudra swain <rudrasankar96@gmail.com>
2025-01-05 21:17:48 +0530

    Print even number updated

--- JS-Conditional/Loop/for-loop/printEvennumber.js
+++ JS-Conditional/Loop/for-loop/printEvennumber.js
@@ -4,6 +4,6 @@
 let i=0;
 for(i=1; i<=50;i++){
     if(i % 2 === 0){
-        console.log('the even numers are', i);
+        console.log('the even numers are', i); // Ans-: 2,4,6,8,10........50
     }
 }
