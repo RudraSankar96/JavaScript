7961dc50f07af13cbc73b387227b969cd9bcdfdd
Rudra swain <rudrasankar96@gmail.com>
2025-01-05 13:08:08 +0530

    5 table using multiplication

---
+++ JS-Conditional/Loop/for-loop/multiplicatiom.js
@@ -0,0 +1,9 @@
+// Multiplication Table:
+// Write a program to print the multiplication table of 5 using a for loop.
+
+let n=5;
+let table=0;
+for(let i=1; i<=10; i++){
+    table=n*i;
+    console.log('the table of 5 is',table);
+}
