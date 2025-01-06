d028c1adb8f4f16f7b3edb5c71afc5996de006b4
Rudra swain <rudrasankar96@gmail.com>
2025-01-06 22:12:35 +0530

    Adding some example

--- JS-Study-from_Appnacollege/Array/Array Methods/Slice- Splice Method/splice.js
+++ JS-Study-from_Appnacollege/Array/Array Methods/Slice- Splice Method/splice.js
@@ -4,3 +4,13 @@ let fruit=['apple','orange','bana','lichi','tomato'];
 fruit.splice(1,2,'chiken','fish');
 console.log(fruit);
 //Output is [ 'apple', 'chiken', 'fish', 'lichi', 'tomato' ]
+
+//delete 2 item from index 2
+fruit.splice(2,2);
+console.log(fruit); 
+//Output is [ 'apple', 'chiken', 'tomato' ]
+
+//Adding element from indx 2.
+fruit.splice(1,0,'banana','orange');
+console.log(fruit);
+// Output is [ 'apple', 'banana', 'orange', 'chiken', 'tomato' ]
