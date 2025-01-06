a2002a0dee558928fdd0cc117bdbddda824e1313
Rudra swain <rudrasankar96@gmail.com>
2025-01-06 16:20:53 +0530

    print all num using for-of loop and conert it small to upeer

---
+++ JS-Study-from_Appnacollege/Array/For-Loop/printNum-for-of.js
@@ -0,0 +1,7 @@
+//Print all array number using for-of loop
+
+let cities=['bombay','delhi','goa','rajsthan'];
+for(let city of cities){
+    console.log(city);
+    console.log(city.toUpperCase()); //Here we can print all the city inti uppercase like-BOMBAY 
+    }
