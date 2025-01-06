ed5507e8054c6c3c29c556e6a775c72318b7af62
Rudra swain <rudrasankar96@gmail.com>
2025-01-06 22:04:38 +0530

    derive from a certin position

---
+++ JS-Study-from_Appnacollege/Array/Array Methods/Slice- Splice Method/slice.js
@@ -0,0 +1,5 @@
+// we can derive a perticular position from an array.
+
+let fruit=['apple','orange','bana','lichi','tomato'];
+console.log(fruit.slice(1,4));
+// Output is [ 'orange', 'bana', 'lichi' ]
---
+++ JS-Study-from_Appnacollege/Array/Array Methods/Slice- Splice Method/splice.js
@@ -0,0 +1,6 @@
+//  Here we can delete and replace an element forn an array by using slice method.
+
+let fruit=['apple','orange','bana','lichi','tomato'];
+fruit.splice(1,2,'chiken','fish');
+console.log(fruit);
+//Output is [ 'apple', 'chiken', 'fish', 'lichi', 'tomato' ]
