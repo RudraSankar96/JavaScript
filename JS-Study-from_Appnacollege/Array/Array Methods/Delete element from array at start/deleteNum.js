53483ba775d933f13418125c18b0e7a76482e910
Rudra swain <rudrasankar96@gmail.com>
2025-01-06 21:36:07 +0530

    Delete a element from start by using shift method

---
+++ JS-Study-from_Appnacollege/Array/Array Methods/Delete element from array at start/deleteNum.js
@@ -0,0 +1,6 @@
+// Delete an element form the start of a array.
+
+let fruit=['apple','orange','bana','lichi','tomato'];
+fruit.shift();
+console.log(fruit);
+// Output is [ 'orange', 'bana', 'lichi', 'tomato' ] here apple delete.
