73b5b134b58896f83b6d0efcfe7b7c95412c2b72
Rudra swain <rudrasankar96@gmail.com>
2025-01-06 20:27:08 +0530

    updated

--- JS-Study-from_Appnacollege/Array/Array convert to String/array_To_string.js
+++
@@ -1,6 +0,0 @@
-// Converting an aray to string....
-
-let fruit=['apple','orange','bana','lichi','tomato'];
-let marks=[23,22,34,45,56];
-console.log(fruit.toString()); //Output is 'apple,orange,bana,lichi,tomato'
-console.log(marks.toString()); //Output is '23,22,34,45,56'
