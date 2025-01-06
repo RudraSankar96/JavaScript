60a5a35db248514b28f637aed258a143d39cd5b8
Rudra swain <rudrasankar96@gmail.com>
2025-01-06 11:50:29 +0530

    Print arry

---
+++ JS-Study-from_Appnacollege/appnaArray.js
@@ -0,0 +1,7 @@
+// Here we can wrie an array and print that.
+let marks=[67,78,89,98,35];
+console.log(marks);   //it prints all the arry member.
+
+//Print all city
+let city=['bomby','delhi','goa'];
+console.log(city);
