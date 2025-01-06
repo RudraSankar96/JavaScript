76da3bf11c94a9e490869b39f1bf31bff4c68507
Rudra swain <rudrasankar96@gmail.com>
2025-01-06 21:30:29 +0530

    Adding new element at the start position of an array

---
+++ JS-Study-from_Appnacollege/Array/Array Methods/Adding element  by Unshift/addNum.js
@@ -0,0 +1,6 @@
+// Here we can add element at the first position of an array by using unshift method.
+
+let fruit=['apple','orange','bana','lichi','tomato'];
+fruit.unshift('guava');
+console.log(); 
+//output is [ 'guava', 'apple', 'orange', 'bana', 'lichi', 'tomato' ]
