c6db516bc6e5af7cd629e0f8982c5675ec82f997
Rudra swain <rudrasankar96@gmail.com>
2025-01-06 19:38:28 +0530

    push a itme inti array

---
+++ JS-Study-from_Appnacollege/Array/Push-Pop_Operation/numPush.js
@@ -0,0 +1,7 @@
+//We can insert single number/item or multiple items at the end of array using pop methos.
+
+let marks=[23,22,34,45,56];
+marks.push(67,78,89,99,80);
+for(let i=0; i<marks.length; i++){
+    console.log(marks[i])
+}
