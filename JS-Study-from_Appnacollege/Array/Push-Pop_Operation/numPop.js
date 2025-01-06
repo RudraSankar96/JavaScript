be05a375133503569754602c577ae25859366e11
Rudra swain <rudrasankar96@gmail.com>
2025-01-06 19:50:19 +0530

    Delete a item from array using pop method

---
+++ JS-Study-from_Appnacollege/Array/Push-Pop_Operation/numPop.js
@@ -0,0 +1,6 @@
+// We can dele a item from the last of array using pop method.
+
+let fruit=['apple','orange','bana','lichi','tomato'];
+fruit.pop();
+console.log(fruit); 
+//here tomato eill delete from array. And output is [ 'apple', 'orange', 'bana', 'lichi' ]
