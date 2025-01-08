9bedee3b758b40b945610f026d0b5dafd563a8b5
Rudra swain <rudrasankar96@gmail.com>
2025-01-08 23:05:23 +0530

    calculator

---
+++ JS-Study-from_Appnacollege/JS-Functions/calculator.js
@@ -0,0 +1,23 @@
+function calculator(a,b,operation){
+    if(operation=== "add"){
+        return a+b;
+    }
+    else if( operation === "sub"){
+        return a-b;
+    }
+    else if(operation === "multiply")
+    {
+        return a*b;
+    }
+    else if(operation === "division"){
+        if(b === 0){
+            return "Error, zero is not divisible"
+        }
+        return a/b;
+    }
+}
+
+let a=45; b=5;
+let operation= "sub";
+let result=calculator(a,b,operation);
+console.log(result);
