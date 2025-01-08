9de1294e386b4454a61fe759c4441531aa22a91a
Rudra swain <rudrasankar96@gmail.com>
2025-01-08 20:49:14 +0530

    updated function

--- JS-Study-from_Appnacollege/JS-Functions/arrowFunc.js
+++ JS-Study-from_Appnacollege/JS-Functions/arrowFunc.js
@@ -19,6 +19,6 @@ const arrSub=(x,y)=>{
     return x-y;
 }
 let x=900; y=899;
-let val=(x-y);
+let val= arrSub(x,y);
 console.log('The rsult of substraction is',val);
 // Output is :- The rsult of substraction is 1
