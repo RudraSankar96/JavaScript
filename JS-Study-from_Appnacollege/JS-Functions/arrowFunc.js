be626e185a87116c4890fd5ea124375d24fa6cd5
Rudra swain <rudrasankar96@gmail.com>
2025-01-08 20:26:21 +0530

    Multplication & Substraction using arrow method

---
+++ JS-Study-from_Appnacollege/JS-Functions/arrowFunc.js
@@ -0,0 +1,24 @@
+// we can multiply anything using arrow function.
+
+function mul(a,b){
+    return a*b;
+}
+const arrowMul=(a,b)=>{
+   console.log(a*b);
+}
+let a=45; b=67;
+let result= mul(a,b);
+console.log('Multiplication of a & b is',result);
+// Output is :- Multiplication of a & b is 3015
+
+//  Do substraction using arrow method
+function sub(x,y){
+    return x-y;
+}
+const arrSub=(x,y)=>{
+    return x-y;
+}
+let x=900; y=899;
+let val=(x-y);
+console.log('The rsult of substraction is',val);
+// Output is :- The rsult of substraction is 1
