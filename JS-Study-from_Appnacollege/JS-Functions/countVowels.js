6941779e4db2f5c9dd6840902f518c73d25f5951
Rudra swain <rudrasankar96@gmail.com>
2025-01-09 20:43:59 +0530

    count vowels

--- JS-Study-from_Appnacollege/JS-Functions/countVowels.js
+++ JS-Study-from_Appnacollege/JS-Functions/countVowels.js
@@ -5,7 +5,7 @@ function countVowels(str){
      let count=0;
      for( let char of str){
         if( char==="a" ||
-            char==="e" || 
+            char==="e" ||  
             char==="i" || 
             char==="o" || 
             char==="u") 
