465a10131db1eab83e35413128e69e07fbed5fc1
Rudra swain <rudrasankar96@gmail.com>
2025-01-11 23:13:32 +0530

    Print square of each eliment of array

---
+++ JS-Study-from_Appnacollege/Array/For-Each loop/squareNum.js
@@ -0,0 +1,12 @@
+// for a given array of numbers print the square of each value using foreach loop.
+
+
+let arr=[2,3,4,5];
+arr.forEach((num)=>{
+console.log(num*num);
+});
+
+// output is 4
+//           9
+//           16
+//           25
