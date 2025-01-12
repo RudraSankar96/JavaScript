f6f9d1d66b44b20c4eebc0fe794fdb972f47ffba
Rudra swain <rudrasankar96@gmail.com>
2025-01-12 20:55:27 +0530

    reverse of string using for loop

---
+++ JS-Conditional/Loop/for-loop/reverseString.js
@@ -0,0 +1,8 @@
+// String Reversal: Use a for loop to reverse a given string.
+// Example: "hello" → "olleh"
+
+let str="hello";
+for(let i=str.length - 1; i >= 0; i--){
+    console.log(str[i]);
+}
+// it prints o l l e h o
