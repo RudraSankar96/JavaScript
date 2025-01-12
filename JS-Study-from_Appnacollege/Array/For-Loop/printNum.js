db73f0d9f26a0ea4dc65a123321c7d89b7555288
Rudra swain <rudrasankar96@gmail.com>
2025-01-12 20:33:35 +0530

    print all array member using for loop

--- JS-Study-from_Appnacollege/Array/For-Loop/printNum.js
+++ JS-Study-from_Appnacollege/Array/For-Loop/printNum.js
@@ -3,3 +3,10 @@ let marks=[60,50,70,80,90,];
 for(i=0;i<marks.length;i++){
     console.log(marks[i]);  //here we can print 60,50,70,80,90
 }
+
+// Array Elements: Given an array let fruits = ["apple", "banana", "cherry"];, use a for loop to print each fruit in the array.
+
+let fruits = ['apple','banana','cherry'];
+for(let i=0;i<fruits.length;i++){
+    console.log(fruits[i]);
+}
