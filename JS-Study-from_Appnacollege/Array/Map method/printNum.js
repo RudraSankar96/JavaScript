2e14fcae509034c5f5243d68276d5cd124bcc4ae
Rudra swain <rudrasankar96@gmail.com>
2025-01-14 20:54:39 +0530

    Using Map method  in array

---
+++ JS-Study-from_Appnacollege/Array/Map method/printNum.js
@@ -0,0 +1,8 @@
+// Print all array number using map method.
+
+let arr=[56,67,78,89,90];
+arr.map((val)=>{
+    console.log(val);
+});
+
+// print the squre of the array numbwr and store it into new array.
