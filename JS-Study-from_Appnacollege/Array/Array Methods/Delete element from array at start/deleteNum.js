e519f91ec289e99000d3568a744428ba12b449a0
Rudra swain <rudrasankar96@gmail.com>
2025-01-15 22:42:49 +0530

    delete operation

--- JS-Study-from_Appnacollege/Array/Array Methods/Delete element from array at start/deleteNum.js
+++ JS-Study-from_Appnacollege/Array/Array Methods/Delete element from array at start/deleteNum.js
@@ -4,3 +4,15 @@ let fruit=['apple','orange','bana','lichi','tomato'];
 fruit.shift();
 console.log(fruit);
 // Output is [ 'orange', 'bana', 'lichi', 'tomato' ] here apple delete.
+
+// 4. JavaScript Array delete Operator
+// The delete operator is used to delete the given value which can be an object, array, or anything.
+
+let emp = { 
+    firstName: "Riya", 
+    lastName: "Kaur", 
+    salary: 40000
+} 
+
+console.log(delete emp.salary); 
+console.log(emp);
