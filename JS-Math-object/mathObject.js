901f60a7bc5db189c5ac7c3b7cf70a35e7075633
Rudra swain <rudrasankar96@gmail.com>
2025-01-18 21:45:13 +0530

    Updated math object js

--- JS-Math-object/mathObject.js
+++ JS-Math-object/mathObject.js
@@ -10,3 +10,35 @@ console.log("Math.SQRT1_2: " + Math.SQRT1_2);
 console.log("Math.LN2: " + Math.LN2);
 console.log("Math.E: " + Math.E);
 console.log("Math.PI: " + Math.PI);
+
+// Output-:
+// Math.LN10: 2.302585092994046
+// Math.LOG2E: 1.4426950408889634
+// Math.Log10E: 0.4342944819032518
+// Math.SQRT2: 1.4142135623730951
+// Math.SQRT1_2: 0.7071067811865476
+// Math.LN2: 0.6931471805599453
+// Math.E: 2.718281828459045
+// Math.PI: 3.141592653589793
+
+// Example 2: Math object methods are used in this example. 
+
+console.log("Math.abs(-4.7): " + Math.abs(-4.7)); 
+2
+console.log("Math.ceil(4.4): " + Math.ceil(4.4)); 
+3
+console.log("Math.floor(4.7): " + Math.floor(4.7)); 
+4
+console.log("Math.sin(90 * Math.PI / 180): " + Math.sin(90 * Math.PI / 180)); 
+6
+console.log("Math.min(0, 150, 30, 20, -8, -200): " + Math.min(0, 150, 30, 20, -8, -200)); 
+8
+console.log("Math.random(): " + Math.random());
+
+// Output
+// Math.abs(-4.7): 4.7
+// Math.ceil(4.4): 5
+// Math.floor(4.7): 4
+// Math.sin(90 * Math.PI / 180): 1
+// Math.min(0, 150, 30, 20, -8, -200): -200
+// Math.random(): 0.7416861489868538
